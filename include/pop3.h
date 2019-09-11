8eb98a9af71d3110afb3d2b583edbd40b7e5a7b6
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-09-11 09:17:05 -0300

    Remove unnecesary commmented structure

--- include/pop3.h
+++ include/pop3.h
@@ -5,19 +5,4 @@
     int servePOP3ConcurrentBlocking(const int server);
     void* handleConnectionPthread(void* args);
 
-    // const char reply_code[][50]={
-	// 	{" \r\n"},  //0
-	// 	{"+OK Welcome\r\n"},   //1
-	// 	{"+OK\r\n"},  //2
-	// 	{"-ERR bad command\r\n"},   //3
-	// 	{"+OK Ready\r\n"},  //4
-	// 	{"+OK Bye\r\n"},  //5
-	// 	{"+OK OK\r\n"},  //6
-	// 	{"+OK User not local;\r\n"},  //7
-	// 	{"+OK Ready\r\n"},  //8
-	// 	{"+OK Bye\r\n"},  //9
-	// 	{"+OK OK\r\n"},  //10
-	// 	{"+OK User not local;\r\n"},  //11
-    // };
-
 #endif
