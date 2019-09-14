aec70151b73065d734563675e171f6a617d6da7e
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-09-14 12:23:14 -0300

    Make server ipv6 compliant

--- include/config.h
+++ include/config.h
@@ -15,9 +15,12 @@ struct config {
     // Target file to redirect stderr when executing a filter
     char* error_file;
 
-    /* IP Addresses */
-    struct sockaddr_in proxy_address;
-    struct sockaddr_in managment_address;
+    /* 
+     * IP Addresses: Using IPv6 because acording to man 7 ipv6 ipv4 connections
+     * can be handled with the v6 API
+     */
+    struct sockaddr_in6 proxy_address;
+    struct sockaddr_in6 managment_address;
 
     /*  Message that is displayed when a part of the message is
      *  replaced because of a filter    
@@ -40,5 +43,6 @@ void change_port(in_port_t *port, char *port_str);
 void change_error_file(char *filename);
 void replace_string(char *previous, char *new);
 void update_config(const int argc, char* const *argv);
+void assign_port(struct sockaddr_in6 *address);
 
 #endif /* CONFIG_H */ 
