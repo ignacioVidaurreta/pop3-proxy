1b248d4af708aaa094b67d10b472d0511b45e3c0
Ignacio Vidaurreta <igvidaurreta@gmail.com>
2019-11-11 16:19:13 -0300

    Add missing method names in header

--- include/cmd_queue.h
+++ include/cmd_queue.h
@@ -15,6 +15,8 @@ typedef struct queue{
 queue* create_queue();
 int add_element(queue* list, void* value);
 int add_elementR(node* curr_node, void* value);
+void* peek(queue* list);
+void* pop(queue* list);
 void free_queue(queue* list);
 void free_node(node* node);
 
